03ee357c4318a34487712e6950738ff203ae5697
MikWhite <mikwhite88@gmail.com>
2022-10-18 16:19:28 +0300

    tictactoe game on servlets and JSP

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,83 @@
+package com.tictactoe;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int index = getSelectedIndex(req);
+        HttpSession currentSession = req.getSession();
+        Field field = extractField(currentSession);
+        Sign currentSign = field.getField().get(index);
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            return;
+        }
+        field.getField().put(index, Sign.CROSS);
+
+        if (checkWin(resp, currentSession, field)) {
+            return;
+        }
+
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        if (emptyFieldIndex >= 0) {
+            field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+        }
+        else {
+            currentSession.setAttribute("draw", true);
+            List<Sign> data = field.getFieldData();
+            currentSession.setAttribute("data", data);
+            resp.sendRedirect("/index.jsp");
+            return;
+        }
+
+        if (checkWin(resp, currentSession, field)) {
+            return;
+        }
+
+        List<Sign> data = field.getFieldData();
+        currentSession.setAttribute("field", field);
+        currentSession.setAttribute("data", data);
+        resp.sendRedirect("/index.jsp");
+
+    }
+
+    private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (Sign.CROSS == winner || Sign.NOUGHT == winner){
+            currentSession.setAttribute("winner", winner);
+            List<Sign> data = field.getFieldData();
+            currentSession.setAttribute("data", data);
+            response.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
+    }
+
+    private Field extractField(HttpSession currentSession){
+    Object field = currentSession.getAttribute("field");
+    if(Field.class != field.getClass()){
+        currentSession.invalidate();
+        throw new RuntimeException("Session is broken, try again");
+    }
+    return (Field) field;
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+}
